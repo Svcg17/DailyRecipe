6b197417259158214beeec950da554752886e231
Sofia Cheung <sofiavictoria17@gmail.com>
2020-08-29 01:30:36 -0700

    Methods now return right away if an error happens when querying data

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -2,12 +2,12 @@ import Recipe from '../models/recipe';
 
 /**
  * Middleware function for GET /api/menu
- * Retrieves all recipes in datbase
+ * Retrieves all recipes in database
  */
 export function getAllRecipes(req, res) {
   Recipe.find({}, (err, recipes) => {
-    if (err) res.status(404).json({ error: 'Menu not found' });
-    else res.status(200).json(recipes);
+    if (err) return res.status(404).json({ error: 'Menu not found' });
+    res.status(200).json(recipes);
   });
 }
 
@@ -20,7 +20,7 @@ export function getRecipe(req, res) {
   const recipeId = req.params.id;
 
   Recipe.findById(recipeId, (err, recipe) => {
-    if (err) res.status(404).json({ error: 'Recipe not found' });
-    else res.status(200).json(recipe);
+    if (err) return res.status(404).json({ error: 'Recipe not found' });
+    res.status(200).json(recipe);
   });
 }
