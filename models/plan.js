f271576a8799c3b644f93dbadb1af41c7eceec62
Sofia Cheung <sofiavictoria17@gmail.com>
2020-08-29 01:22:12 -0700

    Changing recipesPerWeek's type to be an array of numbers

--- models/plan.js
+++ models/plan.js
@@ -1,12 +1,11 @@
 import mongoose from 'mongoose';
 
 const { Schema } = mongoose;
-
 const PlanSchema = new Schema(
   {
     name: String,
     diet: { type: String, enum: ['meat and vegetables', 'vegetables'] },
-    recipesPerWeek: { type: Number, default: 2 },
+    recipesPerWeek: [Number],
     servings: { type: Number, enum: [2, 4] },
     pricePerServing: Number,
     menu: [{ type: Schema.Types.ObjectId, ref: 'Recipe' }],
