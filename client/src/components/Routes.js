c27bd8780e58e4e67c3252726b3fcdb5ce05d7a8
Sofia Cheung <sofiavictoria17@gmail.com>
2020-09-14 01:00:21 -0700

    Integrating a history feature to navigate website easily and adding more layout paths

--- client/src/components/Routes.js
+++ client/src/components/Routes.js
@@ -1,32 +1,33 @@
 import React from 'react';
-import { Switch, BrowserRouter } from 'react-router-dom';
-import { SignInForm, Billing } from './Register/Register';
+import { Switch, Router } from 'react-router-dom';
+import SignUp from './Register/SignUp';
+import DeliveryForm from './Register/Delivery';
+import ChoosePlan from './Register/ChoosePlan';
+import Home from './Public/Home';
+import { createBrowserHistory as createHistory } from 'history'
 
 import PublicLayoutRoute from './Public/PublicLayout';
 import RegisterLayoutRoute from './Register/RegisterLayout';
 
-const Home = () => (
-    <>
-      <p>Homepage</p>
-    </>
-);
+const history = createHistory();
 
 const Menu = () => (
   <>
-  <div>MENU</div>
+    <div>MENU</div>
   </>
 );
 
 const Routes = () => (
-  <BrowserRouter>
+  <Router history={history}>
     <Switch>
       <PublicLayoutRoute exact path='/' component={Home} />
       <PublicLayoutRoute exact path='/menu' component={Menu} />
 
-      <RegisterLayoutRoute exact path='/signin' component={SignInForm} />
-      <RegisterLayoutRoute exact path='/billing' component={Billing} />
+      <RegisterLayoutRoute exact path='/signin' component={SignUp} />
+      <RegisterLayoutRoute exact path='/delivery' component={DeliveryForm} />
+      <RegisterLayoutRoute exact path='/choosePlan' component={ChoosePlan} />
     </Switch>
-  </BrowserRouter>
+  </Router>
 );
 
 export default Routes;
