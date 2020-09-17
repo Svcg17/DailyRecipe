15a6f34abe194244a9cedc7722a106f6a6879b2e
Sofia Cheung <sofiavictoria17@gmail.com>
2020-09-17 02:13:29 -0700

    Moving history definition to Routes component and adding more routes with public layout

--- client/src/components/Routes.js
+++ client/src/components/Routes.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
 import { Switch, Router } from 'react-router-dom';
+import { createBrowserHistory as createHistory } from 'history';
 import SignUp from './Register/SignUp';
 import DeliveryForm from './Register/Delivery';
 import ChoosePlan from './Register/ChoosePlan';
+import LogIn from './Public/LogIn';
+import LogOut from './Public/LogOut';
 import Home from './Public/Home';
-import { createBrowserHistory as createHistory } from 'history'
+// import UserContext from '../UserContext';
 
 import PublicLayoutRoute from './Public/PublicLayout';
 import RegisterLayoutRoute from './Register/RegisterLayout';
-
-const history = createHistory();
 
 const Menu = () => (
   <>
@@ -17,17 +18,25 @@ const Menu = () => (
   </>
 );
 
-const Routes = () => (
-  <Router history={history}>
-    <Switch>
-      <PublicLayoutRoute exact path='/' component={Home} />
-      <PublicLayoutRoute exact path='/menu' component={Menu} />
+const Routes = () => {
+  const history = createHistory();
+  /* const [user, setUser] = useState(null);
+  const providerValue = useMemo(() => ({ user, setUser }), [user, setUser]); */
 
-      <RegisterLayoutRoute exact path='/signin' component={SignUp} />
-      <RegisterLayoutRoute exact path='/delivery' component={DeliveryForm} />
-      <RegisterLayoutRoute exact path='/choosePlan' component={ChoosePlan} />
-    </Switch>
-  </Router>
-);
+  return (
+      <Router history={history}>
+          <Switch>
+            <PublicLayoutRoute exact path='/' component={Home} />
+            <PublicLayoutRoute exact path='/menu' component={Menu} />
+            <PublicLayoutRoute exact path='/login' component={LogIn} />
+            <PublicLayoutRoute exact path='/logout' component={LogOut} />
+
+            <RegisterLayoutRoute exact path='/signin' component={SignUp} />
+            <RegisterLayoutRoute exact path='/delivery' component={DeliveryForm} />
+            <RegisterLayoutRoute exact path='/choosePlan' component={ChoosePlan} />
+          </Switch>
+      </Router>
+  );
+};
 
 export default Routes;
