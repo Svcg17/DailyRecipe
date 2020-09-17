85ad247e738d2a1d191d21cec968647fb1634a71
Sofia Cheung <sofiavictoria17@gmail.com>
2020-09-17 02:12:23 -0700

    Adding base code for a useContext hook

--- client/src/components/Public/Header.js
+++ client/src/components/Public/Header.js
@@ -1,25 +1,38 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import { Link } from 'react-router-dom';
+// import UserContext from '../../UserContext';
 
-const Header = () => (
-  <div className='container'>
-    <Navbar expand='lg'>
-      <Navbar.Brand as={Link} to='/'>Daily Recipe Logo</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="navbar">
-        <Nav className="mr-auto">
-          <Nav.Link as={Link} to='/menu'>Explore Menu</Nav.Link>
-          <Nav.Link as={Link} to='/pricing'>Pricing</Nav.Link>
-        </Nav>
-        <Nav>
-          <Nav.Link as={Link} to='/signin'>Sign Up</Nav.Link>
-          <Nav.Link as={Link} to='/login'>Log In</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  </div>
-);
+const Header = () => {
+  // const { user } = useContext(UserContext);
+  const user = null;
+
+  return (
+    <div className='container'>
+      <Navbar expand='lg'>
+        <Navbar.Brand as={Link} to='/'>Daily Recipe Logo</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="navbar">
+          <Nav className="mr-auto">
+            <Nav.Link as={Link} to='/menu'>Explore Menu</Nav.Link>
+            <Nav.Link as={Link} to='/pricing'>Pricing</Nav.Link>
+          </Nav>
+          {user ? (
+            <Nav>
+              <Nav.Link as={Link} to='/profile'>Hi {user.name}</Nav.Link>
+              <Nav.Link as={Link} to='/logout'>Logout</Nav.Link>
+            </Nav>
+          ) : (
+            <Nav>
+              <Nav.Link as={Link} to='/signin'>Sign Up</Nav.Link>
+              <Nav.Link as={Link} to='/login'>Log In</Nav.Link>
+            </Nav>
+          )}
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
+};
 
 export default Header;
