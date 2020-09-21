e7f05892dc1fe3ccf7d35a9f32899db671d87ede
Sofia Cheung <sofiavictoria17@gmail.com>
2020-09-21 11:33:43 -0700

    Refactor as a responsive and collapsable navbar

--- client/src/components/Layouts/Public/Header.js
+++ client/src/components/Layouts/Public/Header.js
@@ -4,33 +4,32 @@ import Nav from 'react-bootstrap/Nav';
 import { Link } from 'react-router-dom';
 import UserContext from '../../../context/UserContext';
 
+/** Public layout's header */
 const Header = () => {
   const { user } = useContext(UserContext);
 
   return (
-    <div className='container'>
-      <Navbar expand='lg'>
-        <Navbar.Brand as={Link} to='/'>Daily Recipe Logo</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="navbar">
-          <Nav className="mr-auto">
-            <Nav.Link as={Link} to='/menu'>Explore Menu</Nav.Link>
-            <Nav.Link as={Link} to='/pricing'>Pricing</Nav.Link>
+    <Navbar collapseOnSelect expand='md' sticky='top'>
+      <Navbar.Brand as={Link} to='/'>Daily Recipe Logo</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="navbar">
+        <Nav className="mr-auto">
+          <Nav.Link as={Link} eventKey='1' to='/menu'>Explore Menu</Nav.Link>
+          <Nav.Link as={Link} eventKey='2' to='/pricing'>Pricing</Nav.Link>
+        </Nav>
+        {user ? (
+          <Nav>
+            <Nav.Link as={Link} eventKey='3' to='/profile'>My Profile</Nav.Link>
+            <Nav.Link as={Link} eventKey='4' to='/logout'>Logout</Nav.Link>
           </Nav>
-          {user ? (
-            <Nav>
-              <Nav.Link as={Link} to='/profile'>Hi {user.name}</Nav.Link>
-              <Nav.Link as={Link} to='/logout'>Logout</Nav.Link>
-            </Nav>
-          ) : (
-            <Nav>
-              <Nav.Link as={Link} to='/signin'>Sign Up</Nav.Link>
-              <Nav.Link as={Link} to='/login'>Log In</Nav.Link>
-            </Nav>
-          )}
-        </Navbar.Collapse>
-      </Navbar>
-    </div>
+        ) : (
+          <Nav className='ml-auto'>
+            <Nav.Link as={Link} eventKey='3' to='/signin'>Sign Up</Nav.Link>
+            <Nav.Link as={Link} eventKey='4' to='/login'>Log In</Nav.Link>
+          </Nav>
+        )}
+      </Navbar.Collapse>
+    </Navbar>
   );
 };
 
