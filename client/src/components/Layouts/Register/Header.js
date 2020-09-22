121ef93504afb77f2203422ba207bf244e923b7b
Sofia Cheung <sofiavictoria17@gmail.com>
2020-09-21 23:27:27 -0700

    Refactor navbar to highlight the tab that matches the current location path

--- client/src/components/Layouts/Register/Header.js
+++ client/src/components/Layouts/Register/Header.js
@@ -1,57 +1,31 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link, useLocation } from 'react-router-dom';
+import './Register.css';
 
 /** Register layout's header */
 const Header = () => {
-  const locationRR = useLocation();
-  const [location, setLocation] = useState('/signin');
-  const [currStep, setCurrStep] = useState('/signin');
-
-  useEffect(() => {
-    const { pathname } = locationRR;
-    setLocation(pathname);
-  }, [locationRR]);
+  const location = useLocation();
+  const steps = {
+    '/signin': 'Sign Up',
+    '/delivery': 'Address and Billing',
+    '/choosePlan': 'Choose Your Plan',
+  };
 
   return (
-   <Navbar>
+   <Navbar collapseOnSelect expand='md'>
     <Nav
-      className='justify-content-center mr-auto'
-      variant='pills'
-      onSelect={(eventKey, event) => {
-        console.log('location', location);
-        console.log(event.target.pathname);
-        setCurrStep(event.target.pathname);
-      }}>
-        <Navbar.Brand className='flex-grow-1' as={Link} to='/'>Daily Recipe Logo</Navbar.Brand>
-        <Nav.Item>
-          <Nav.Link
-            as={Link}
-            to='/signin'
-            value='/signin'
-            eventKey='1'
-            className={location === currStep ? 'active' : 'disabled'}
-          >Sign up</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link
-            as={Link}
-            to='/delivery'
-            value='/delivery'
-            eventKey='2'
-            className={location === currStep ? 'active' : 'disabled'}
-          >Address and Billing</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link
-            as={Link}
-            to='/choosePlan'
-            value='/choosePlan'
-            eventKey='3'
-            className={location === currStep ? 'active' : 'disabled'}
-          >Choose your Plan</Nav.Link>
-        </Nav.Item>
+      className='mx-auto'
+      variant='pills'>
+        <Navbar.Brand as={Link} to='/'>Daily Recipe Logo</Navbar.Brand>
+        {Object.keys(steps).map((key, idx) => (
+          <Nav.Item key={idx} className='text-center'>
+            <Nav.Link
+              className={location.pathname === key ? 'active' : 'disabled'}
+            >{steps[key]}</Nav.Link>
+          </Nav.Item>
+        ))}
     </Nav>
   </Navbar>
   );
