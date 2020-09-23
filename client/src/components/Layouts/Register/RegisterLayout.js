8051d01722146bcc93c11000952ad97a086ce6ed
Sofia Cheung <sofiavictoria17@gmail.com>
2020-09-22 23:55:38 -0700

    Integrating Stripe's Elements context across routes with a register layout

--- client/src/components/Layouts/Register/RegisterLayout.js
+++ client/src/components/Layouts/Register/RegisterLayout.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
 import Header from './Header';
+
+const stripePromise = loadStripe('pk_test_51HRpxMFseyHrAR6RPpGwfSddqA1w7v87HBP8Ew3Xm7BN4iqcc1yDJoxWIXMgkt6KfURcHzKGFCJhJKgGQqldNU3j00kcWOtSBC');
 
 const RegisterLayout = ({ children }) => (
   <Container>
@@ -13,7 +17,9 @@ const RegisterLayout = ({ children }) => (
 const RegisterLayoutRoute = ({ component: Component, ...rest }) => (
   <Route {...rest} render={(matchProps) => (
     <RegisterLayout>
-      <Component {...matchProps} />
+      <Elements stripe={stripePromise}>
+        <Component {...matchProps} />
+      </Elements>
     </RegisterLayout>
   )} />
 );
