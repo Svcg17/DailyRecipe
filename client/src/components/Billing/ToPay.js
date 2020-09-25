e6c86f1139655d924a706ae12cbb00c6d06c7532
Sofia Cheung <sofiavictoria17@gmail.com>
2020-09-24 23:44:14 -0700

    Improving structure and styling of ToPlay's card

--- client/src/components/Billing/ToPay.js
+++ client/src/components/Billing/ToPay.js
@@ -2,40 +2,46 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import ListGroup from 'react-bootstrap/ListGroup';
 import { Cookies } from 'react-cookie';
 
 /** Display a user's order information before payment */
 const ToPay = () => {
   const cookies = new Cookies();
   const userPlan = cookies.get('userPlan'); // retrive cookie with plan's data
+  const deliveryDate = new Date(Date.now() + (7 * 24 * 60 * 60 * 1000));
 
   return (
-    <Card className='mb-2'>
+    <Card className='mb-2 orderInfo'>
       <Card.Header>Order Information</Card.Header>
       <Card.Body as={Row}>
-        <Col xs={6}>
-          <Card.Text>{userPlan.plan.name} Plan </Card.Text>
-          <Card.Text>
-            {userPlan.recipesPerWeek} recipes per week for {userPlan.plan.servings} people
-          </Card.Text>
-          <Card.Text>
-            First delivery date 7 days from now *
-          </Card.Text>
+        <Col xs={12} md={6} className='planDetails'>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>Plan: {userPlan.plan.name}</ListGroup.Item>
+            <ListGroup.Item>
+              {userPlan.recipesPerWeek} recipes per week for {userPlan.plan.servings} people
+            </ListGroup.Item>
+            <ListGroup.Item>
+              {`First delivery arrives ${deliveryDate.getMonth() + 1}/${deliveryDate.getDate()}/${deliveryDate.getFullYear()}`}
+            </ListGroup.Item>
+          </ListGroup>
         </Col>
-        <Card.Text as={Col} xs={6}>
-          <Row>
-            <Col xs={6}>Subtotal</Col>
-            <Col xs={6}>{userPlan.totalPrice}</Col>
-          </Row>
-          <Row>
-            <Col xs={6}>Shipping</Col>
-            <Col xs={6}>Free</Col>
-          </Row>
-          <Row>
-            <Col xs={6}>Total</Col>
-            <strong as={Col} xs={6}>{userPlan.totalPrice}</strong>
-          </Row>
-        </Card.Text>
+        <Col xs={12} md={6} className='payDetails border-top'>
+          <ListGroup variant='flush'>
+            <ListGroup.Item className='d-flex'>
+              <Col xs={6}>Subtotal</Col>
+              <Col xs={6} className='text-right'>{userPlan.totalPrice}</Col>
+            </ListGroup.Item>
+            <ListGroup.Item className='d-flex'>
+              <Col xs={6}>Shipping</Col>
+              <Col xs={6} className='text-right'>Free</Col>
+            </ListGroup.Item>
+            <ListGroup.Item className='d-flex'>
+              <Col xs={6}>Total</Col>
+              <Col className='font-weight-bold text-right' xs={6}>{userPlan.totalPrice}</Col>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
       </Card.Body>
     </Card>
   );
