6b3ac4fdfb6b3dda97d9603dadff3334b2325546
Sofia Cheung <sofiavictoria17@gmail.com>
2020-10-04 16:41:59 -0700

    Updating App's function syntax

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Routes from './components/Routes';
 
-function App() {
-  return (
-    <div className="App">
-      <Routes />
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Routes />
+  </div>
+);
 
 export default App;
