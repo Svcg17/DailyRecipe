1ef0956f9c009019d482a19928c22e962171c4e3
Sofia Cheung <sofiavictoria17@gmail.com>
2020-10-08 01:15:42 -0700

    Have the carousel be a separate component and adding more styling

--- client/src/components/User/ThisWeek/ThisWeek.js
+++ client/src/components/User/ThisWeek/ThisWeek.js
@@ -3,15 +3,14 @@ import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Image from 'react-bootstrap/Image';
 import Carousel from 'react-bootstrap/Carousel';
 import CardDeck from 'react-bootstrap/CardDeck';
-import Card from 'react-bootstrap/Card';
 
-import './thisWeek.css';
+import RecipeCard from '../../Recipe/RecipeCard';
 import carousel1 from '../../../assets/carousel_1.jpg';
 import carousel2 from '../../../assets/carousel_2.jpg';
 import carousel3 from '../../../assets/carousel_3.jpg';
+import './thisWeek.css';
 
 /** Displays a carousel and the user's selected recipes */
 const ThisWeek = ({ history }) => {
@@ -37,77 +36,80 @@ const ThisWeek = ({ history }) => {
       .catch((error) => setError(error.toString()));
   }, []);
 
+  const InfoCarousel = () => (
+    <Carousel className='infoCarousel'>
+      <Carousel.Item>
+        <img
+          className='d-block w-100'
+          src={carousel1}
+          height='400'
+          alt='First slide'
+        />
+        <Carousel.Caption>
+          <div className='innerCaption'>
+            <h3>Don't miss our latests blogs and news articles</h3>
+            <p>Subscribe to our newletter and become part of our culinary community</p>
+          </div>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={carousel2}
+          height='400'
+          alt="Third slide"
+        />
+        <Carousel.Caption>
+          <h3>All you have to do is wait now</h3>
+          <p>Your order will soon be processed, shipped and delivered right to your door</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={carousel3}
+          height='400'
+          alt="Third slide"
+        />
+        <Carousel.Caption>
+          <h3>We love new ideas</h3>
+          <p>Got any recipe ideas you would love to share? At Daily Recipe, we always have to come up with new recipes so share them with us!</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+
   return (
     <Container className='my-4'>
-      <h1 className='my-2'>This week's delivery</h1>
-      <Carousel className='infoCarousel'>
-        <Carousel.Item>
-          <img
-            className='d-block w-100'
-            src={carousel1}
-            height='400'
-            alt='First slide'
-          />
-          <Carousel.Caption>
-            <div className='innerCaption'>
-              <h3>Don't miss our latests blogs and news articles</h3>
-              <p>Subscribe to our newletter and become part of our culinary community</p>
-            </div>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={carousel2}
-            height='400'
-            alt="Third slide"
-          />
-          <Carousel.Caption>
-            <h3>All you have to do is wait now</h3>
-            <p>Your order will soon be processed, shipped and delivered right to your door</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src={carousel3}
-            height='400'
-            alt="Third slide"
-          />
-          <Carousel.Caption>
-            <h3>We love new ideas</h3>
-            <p>Got any recipe ideas you would love to share? At Daily Recipe, we always have to come up with new recipes so share them with us!</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
+      <h1 className='my-2'>This Week's Delivery</h1>
+      <InfoCarousel />
       <Row className='my-5'>
         <Col>
-          <div className='weekInfo'>
-            <div className='d-flex flex-column'>
-              <span>Arriving</span>
-              <span>
-                {`${deliveryDate.getMonth() + 1}/${deliveryDate.getDate()}/${deliveryDate.getFullYear()}`}
-              </span>
-            </div>
-            <Button onClick={() => history.push('/user/upcoming')}>Manage deliveries</Button>
-          </div>
-          <CardDeck as={Row} className='justify-content-center'>
-          {error && <Col xs={12} className='invalid-feedback d-block' role='alert'>{error}</Col>}
-          {recipes.length && recipes.map((recipe) => (
-            <Col xs={12} md={8} lg={6} xl={4} className='cardWrapper p-lg-0' key={`col-${recipe.name}`}>
-              <Card key={recipe.title} id={recipe._id}>
-                <Image fluid src={`https://source.unsplash.com/random/358x300/?${recipe.title}`} />
-                    <Card.Body>
-                    <Card.Title>{recipe.title}</Card.Title>
-                      <div className='d-flex justify-content-between'>
-                        <span>{recipe.duration}</span>
-                        <Button onClick={() => history.push(`/recipes/:${recipe._id}`)}>Details</Button>
-                      </div>
-                    </Card.Body>
-              </Card>
+          {!recipes.length && (
+            <Col className='emptyWeek mx-auto'>
+              <p>You currently have no recipes coming your way. Go ahead and click the button to select the recipes you want delivered for this week</p>
+              <Button onClick={() => history.push('/user/upcoming')}>Choose Upcoming Recipes</Button>
             </Col>
-          ))}
-        </CardDeck>
+          )}
+          {error && <Col xs={12} className='invalid-feedback d-block' role='alert'>{error}</Col>}
+          {recipes.length && (
+            <>
+              <div className='weekInfo'>
+                <div className='d-flex flex-column'>
+                  <span>Arriving</span>
+                  <span>
+                    {`${deliveryDate.getMonth() + 1}/${deliveryDate.getDate()}/${deliveryDate.getFullYear()}`}
+                  </span>
+                </div>
+                <Button onClick={() => history.push('/user/upcoming')}>Manage deliveries</Button>
+              </div>
+              <CardDeck as={Row} className='justify-content-center'>
+              {recipes.map((recipe) => (
+                <RecipeCard recipe={recipe} history={history} />
+              ))}
+              </CardDeck>
+            </>
+          )}
         </Col>
       </Row>
     </Container>
