3eba9cb5611ba95b9c56eadc0ac116ab41be5379
Sofia Cheung <sofiavictoria17@gmail.com>
2020-10-18 22:51:28 -0700

    Wrapping children props with a div tag

--- client/src/components/Layouts/Public/PublicLayout.js
+++ client/src/components/Layouts/Public/PublicLayout.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Header from './Header';
+import Footer from '../../Footer/Footer';
+
+import './public.css';
 
 const PublicLayout = ({ children }) => (
   <div>
     <Header />
-    {children}
+      <div className='body'>
+        {children}
+      </div>
+    <Footer />
   </div>
 );
 
