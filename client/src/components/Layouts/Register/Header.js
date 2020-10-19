feb6e759afad0ab1d3c0d0a5ae330d6f287bde2e
Sofia Cheung <sofiavictoria17@gmail.com>
2020-10-18 22:26:01 -0700

    Adding logo to navigation bar

--- client/src/components/Layouts/Register/Header.js
+++ client/src/components/Layouts/Register/Header.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Image from 'react-bootstrap/Image';
 import { Link, useLocation } from 'react-router-dom';
+
+import Logo from '../../../assets/Logo.png';
 
 /** Register layout's header */
 const Header = () => {
@@ -15,11 +18,11 @@ const Header = () => {
   return (
    <Navbar collapseOnSelect expand='md'>
     <Nav
-      className='mx-auto'
+      className='mx-auto align-items-center'
       variant='pills'>
-        <Navbar.Brand as={Link} to='/'>Daily Recipe Logo</Navbar.Brand>
+        <Navbar.Brand as={Link} to='/'><Image src={Logo} width='200' /></Navbar.Brand>
         {Object.keys(steps).map((key, idx) => (
-          <Nav.Item key={idx} className='text-center'>
+          <Nav.Item key={idx}>
             <Nav.Link
               className={location.pathname === key ? 'active' : 'disabled'}
             >{steps[key]}</Nav.Link>
