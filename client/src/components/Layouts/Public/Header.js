74248e3db8cb6d2be1318a05a708ce9de40bbdb9
Sofia Cheung <sofiavictoria17@gmail.com>
2020-10-18 22:54:37 -0700

    Organizing link's eventKeys, adding logo to navigation bar and adding method to recognize active link

--- client/src/components/Layouts/Public/Header.js
+++ client/src/components/Layouts/Public/Header.js
@@ -1,35 +1,42 @@
 import React, { useContext } from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import { Link } from 'react-router-dom';
+import Image from 'react-bootstrap/Image';
+import { Link, useLocation } from 'react-router-dom';
+
 import UserContext from '../../../context/UserContext';
+import './public.css';
+import Logo from '../../../assets/Logo.png';
 
 /** Public layout's header */
 const Header = () => {
   const { user } = useContext(UserContext);
+  const location = useLocation();
+
+  const isActive = (path) => (location.pathname === path ? 'activeLink' : '');
 
   return (
-    <Navbar collapseOnSelect expand='md'>
-      <Navbar.Brand as={Link} to='/'>Daily Recipe Logo</Navbar.Brand>
+    <Navbar sticky='top' className='publicHeader' collapseOnSelect expand='md'>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Brand as={Link} to='/'><Image src={Logo} alt='Daily Recipe Logo' /></Navbar.Brand>
       <Navbar.Collapse id="navbar">
-        <Nav className="mr-auto">
-          {user && <Nav.Link as={Link} eventKey='6' to='/user/thisWeek'>This Week</Nav.Link>}
-          {user && <Nav.Link as={Link} eventKey='5' to='/user/upcoming'>Upcoming</Nav.Link>}
-          <Nav.Link as={Link} eventKey='1' to='/menu'>Explore Menu</Nav.Link>
-          <Nav.Link as={Link} eventKey='2' to='/pricing'>Pricing</Nav.Link>
-        </Nav>
-        {user ? (
-          <Nav>
-            <Nav.Link as={Link} eventKey='3' to='user/profile'>My Profile</Nav.Link>
-            <Nav.Link as={Link} eventKey='4' to='/logout'>Logout</Nav.Link>
+        <Nav>
+          {user && <Nav.Link as={Link} eventKey='1' to='/user/thisWeek' className={isActive('/user/thisWeek')}>This Week</Nav.Link>}
+          {user && <Nav.Link as={Link} eventKey='2' to='/user/upcoming' className={isActive('/user/upcoming')}>Upcoming</Nav.Link>}
+          <Nav.Link as={Link} eventKey='3' to='/menu' className={isActive('/menu')}>Explore Menu</Nav.Link>
+          <Nav.Link as={Link} eventKey='4' to='/pricing' className={isActive('/pricing')}>Pricing</Nav.Link>
+          {user ? (
+            <div className='d-md-flex ml-md-auto'>
+              <Nav.Link as={Link} eventKey='5' to='/user/profile' className={isActive('/user/profile')}>My Profile</Nav.Link>
+              <Nav.Link as={Link} eventKey='6' to='/logout' className={isActive('/logout')}>Logout</Nav.Link>
+            </div>
+          ) : (
+            <div className='d-md-flex ml-md-auto'>
+              <Nav.Link as={Link} eventKey='7' to='/signin' className={isActive('/signin')}>Sign Up</Nav.Link>
+              <Nav.Link as={Link} eventKey='8' to='/login' className={isActive('/login')}>Log In</Nav.Link>
+            </div>
+          )}
           </Nav>
-        ) : (
-          <Nav className='ml-auto'>
-            <Nav.Link as={Link} eventKey='3' to='/signin'>Sign Up</Nav.Link>
-            <Nav.Link as={Link} eventKey='4' to='/login'>Log In</Nav.Link>
-          </Nav>
-        )}
       </Navbar.Collapse>
     </Navbar>
   );
