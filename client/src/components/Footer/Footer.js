c122b7d47cab3074993c33e4dc0503c0bf6884fd
The Modes Empire <58116877+journne@users.noreply.github.com>
2020-11-08 11:54:29 -0800

    Footer, Color and Header Text Change

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -14,8 +14,8 @@ const Footer = ({ history }) => (
     <Row className='footerInfo'>
       <Col xs={12} md={6} lg={4} className='description'>
         <Image src={Logo} width='200' />
-        <small>An ingredient and recipe delivery service website</small>
-        <small>Made with ♥ in San Fransisco, CA</small>
+        <small>An ingredient and recipe delivery service</small>
+        <small>Made with ♥ in Lagos, Nigeria</small>
       </Col>
       <Col xs={6} md={3} lg={4} className='footerLinks'>
         <Nav>
@@ -29,18 +29,18 @@ const Footer = ({ history }) => (
         <div>About Me</div>
         <div>
           <span className='socialIcon'>
-            <a href='https://twitter.com/sofivism'>
+            <a href='https://twitter.com/eliteschef'>
               <i className='fa fa-twitter'></i>
             </a>
           </span>
           <span className='socialIcon'>
-            <a href='https://github.com/Svcg17'>
-              <i className='fa fa-github'></i>
+            <a href='https://github.com/eliteschef'>
+              <i className='fa fa-instagram'></i>
             </a>
           </span>
           <span className='socialIcon'>
-            <a href='https://www.linkedin.com/in/sofiacheung/'>
-              <i className='fa fa-linkedin'></i>
+            <a href='https://www.facebook.com/eliteschef/'>
+              <i className='fa fa-facebook'></i>
             </a>
           </span>
         </div>
@@ -48,7 +48,7 @@ const Footer = ({ history }) => (
     </Row>
     <Row>
       <Col xs={12} className='license mx-auto'>
-        <small>© By Sofia Cheung 2020</small>
+        <small>© By The Modes Empire 2020</small>
       </Col>
     </Row>
   </Container>
