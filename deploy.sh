009c0f2fcea42809df3f322cb8a952bb0b185ea7
lnvc <laira.cham@obf.ateneo.edu>
2020-11-09 21:02:36 +0800

    deleted extra files

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
+git checkout master
+git reset --hard origin/master
+nvm use node
+chown -R ubuntu:ubuntu /var/www/html/
 cd /var/www/html
 git pull origin master
+rm -rf node_modules/
 yarn install
 cd client
+rm -rf node_modules/
 yarn install
 yarn run build
 cd ..
